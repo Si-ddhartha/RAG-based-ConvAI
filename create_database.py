fdaeb57bf0755999c7039dff4814ea39a9ef1ad1
Aniket <74449359+Si-ddhartha@users.noreply.github.com>
2024-09-02 14:59:58 +0530

    Created file to generate text embeddings from pdf

---
+++ create_database.py
@@ -0,0 +1,65 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.schema import Document
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+
+import google.generativeai as genai
+
+from dotenv import load_dotenv
+
+import os
+import shutil
+
+load_dotenv()
+GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
+genai.configure(api_key=GOOGLE_API_KEY)
+
+DATA_PATH = 'data/IFRC FIRST AID GUIDE 2020.pdf'
+CHROMA_PATH = 'chroma'
+
+def main():
+    generate_database()
+
+def generate_database():
+    documents = load_documents()
+    chunks = split_text(documents)
+    save_to_chroma(chunks)
+
+def load_documents():
+    loader = PyPDFLoader(DATA_PATH)
+    docs = loader.load()
+
+    return docs
+
+def split_text(documents: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size = 500,
+        chunk_overlap = 100,
+        length_function = len,
+        add_start_index = True
+    )
+
+    chunks = text_splitter.split_documents(documents)
+    print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
+
+    return chunks
+
+def save_to_chroma(chunks: list[Document]):
+    if os.path.exists(CHROMA_PATH):
+        shutil.rmtree(CHROMA_PATH)
+
+    embedding_model = GoogleGenerativeAIEmbeddings(
+        model="models/text-embedding-004",
+        task_type="RETRIEVAL_DOCUMENT"
+    )
+
+    db = Chroma.from_documents(
+        chunks, embedding_model, persist_directory=CHROMA_PATH
+    )
+
+    db.persist()
+    print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
+
+if __name__ == '__main__':
+    main()
